2970fe47111bc845f52e79c99008f0f850b26c5c
Luca Parolini <lucapar@gmail.com>
2018-12-02 22:07:56 +0100

    Working example

--- regex_tester.py
+++ regex_tester.py
@@ -1,2 +1,21 @@
+import re
+
+def make_all_single_lines(in_string):
+    pattern = "\\\W*(?:\r\n|\n|\r)"
+    repl = ""
+    out_string = re.sub(pattern, repl, in_string)
+    return out_string
+
+def get_import_lines(in_string):
+    pattern = "(?<=import\W).+"
+    return re.findall(pattern, in_string)
+
+def get_import_items(in_list):
+    all_items = []
+    pattern = "\w+"
+    for i_string in in_list:
+        all_items.extend([i_match for i_match in re.findall(pattern, i_string)])
+    return list(set(all_items)).sort()
+
 def extract_python_lib(in_string):
-    return []
+    return get_import_items(get_import_lines(make_all_single_lines(in_string)))
--- test/python_library_test.py
+++ test/python_library_test.py
@@ -1,5 +1,6 @@
 import unittest
-from python_regex_tester.regex_tester import extract_python_lib
+from python_regex_tester.regex_tester import extract_python_lib, \
+    make_all_single_lines, get_import_lines, get_import_items
 
 
 class PythonLibraryTest(unittest.TestCase):
@@ -14,6 +15,35 @@ import os
 import sys
 import re
 
+import urllib2, base64,     json, os, sys,      re
+import (urllib2, base64,     json, os, sys,      re)
+import (urllib2, base64,     json, os, sys, \
+        re)
+
+os.system("clear")
+print "-" * 80
+print "Command Line Search Tool"
+print "-" * 80
+
+def Banner(text):
+    print "=" * 70
+    print text
+    print "=" * 70
+    sys.stdout.flush()
+"""
+        self.in_data_all_one_lines = """
+#!/usr/bin/env python27
+import urllib2
+import base64
+import json
+import os
+import sys
+import re
+
+import urllib2, base64,     json, os, sys,      re
+import (urllib2, base64,     json, os, sys,      re)
+import (urllib2, base64,     json, os, sys,         re)
+
 os.system("clear")
 print "-" * 80
 print "Command Line Search Tool"
@@ -26,18 +56,41 @@ def Banner(text):
     sys.stdout.flush()
 """
 
-    def test_extract_library_when_typical(self):
-        expected_library = [
+        self.in_data_import_lines = [
             "urllib2",
-            "ase64",
+            "base64",
             "json",
             "os",
             "sys",
-            "re"
-        ]
+            "re",
+            "urllib2, base64,     json, os, sys,      re",
+            "(urllib2, base64,     json, os, sys,      re)",
+            "(urllib2, base64,     json, os, sys,         re)"]
+
+        self.in_data_import_items = [
+            "urllib2",
+            "base64",
+            "json",
+            "os",
+            "sys",
+            "re"].sort()
+
+    def test_make_all_single_lines(self):
+        expected_text = self.in_data_all_one_lines
+        self.assertEqual(expected_text, make_all_single_lines(self.in_data))
+
+    def test_get_import_lines(self):
+        expected_text = self.in_data_import_lines
+        self.assertEqual(expected_text, get_import_lines(self.in_data_all_one_lines))
+
+    def test_get_import_items(self):
+        expected_text = self.in_data_import_items
+        self.assertEqual(expected_text, get_import_items(self.in_data_import_lines))
+
+    def test_extract_library_when_typical(self):
+        expected_library = self.in_data_import_items
         out_list = extract_python_lib(self.in_data)
         self.assertEqual(expected_library, out_list)
-
 
 if __name__ == '__main__':
     unittest.main()
